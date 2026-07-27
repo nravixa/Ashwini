44667888dee54ad2d8c4a72f78e97ad6202d4325
Neha Raut <your-github-email@example.com>
2026-07-27 11:26:38 +0530

    Last changes

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import SEO from "../components/SEO";
 import Image from "../components/Image";
